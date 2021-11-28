6ec2099cfc66825206a35f676054067a7de9ae90
Dmitrii Vazanov <lord1of2ultima@gmail.com>
2021-11-28 21:39:16 +0300

    Добавьте файлы проекта.

---
+++ OperatorBot/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;//Пакет JSON
+using OperatorBot;
+
+namespace OperatorBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Responser responser = new Responser("9519997810", "31spFKX5", "37065");
+            responser.Authenticate() ;
+        }
+    }
+}
---
+++ OperatorBot/Responser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;//Пакет JSON
+using Newtonsoft.Json.Linq;
+
+namespace OperatorBot
+{
+
+    public class Responser
+    {
+        public string msidn { get; set; }
+        public string password { get; set; }
+        public string employer { get; set; }
+        public string BToken { get; set; }
+
+        public Responser(string msidn, string password, string employer)
+        {
+            this.msidn = msidn;
+            this.password = password;
+            this.employer = employer;
+        }
+        public void Authenticate()
+        {
+            //Вернёт employers если он не указан, или Bearer token
+            WebRequest request = WebRequest.Create("https://art.taxi.mos.ru/api/authenticate");
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            var postData = $"msisdn={msidn}";
+            postData += $"&password={password}";
+
+            if (!string.IsNullOrEmpty(employer))
+                postData += $"&employeeId={employer}";
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            if (!string.IsNullOrEmpty(employer))
+                BToken = JObject.Parse(responseString).SelectToken("token").ToString().Replace("Bearer ","");
+            else
+            {
+               // var a = JObject.Parse(responseString).SelectToken("token").ToString();
+            }
+            Console.WriteLine(responseString);
+            Console.ReadLine();
+        }
+    }
+}
